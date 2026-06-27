1a52c66f95b5780370d655740ce6908a4da29551
kiracardam-rgb <kiracardam@gmail.com>
2026-06-27 15:08:52 +0100

    Add files via upload

---
+++ Heat_loss_calculator.py
@@ -0,0 +1,6 @@
+u_value = float(input("What is your u_value"))
+temperature_difference= float(input("What is your temperature difference?"))
+area =  float(input("What is your area?"))
+
+heat_loss = u_value * temperature_difference * area
+print ("The value of your heat loss is",heat_loss)
